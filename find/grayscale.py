bce0f80140655a7891fe8cb8148c07740d5a003b
Alex Kesling <alex.kesling@gmail.com>
2013-03-06 00:17:46 -0500

    Working generic OpenCL grayscale... WOO!

--- find/grayscale.py
+++ find/grayscale.py
@@ -1,12 +1,15 @@
 from pylab import *
+import Image
 import pyopencl as cl
-import cairo
+import sys
+import os.path
 
-# Imread flips the y-axis, so we have to flip it back...
-original = imread('../data/over_exposed_butner.jpg')[::-1, :, ::-1]
-original = np.dstack(
-    (original, 255*ones(original.shape[:2], dtype=np.uint8))
-    )#.copy()
+if not len(sys.argv) > 1:
+    exit('Rage quit!')
+
+target_file = sys.argv[1]
+
+original = array(Image.open(target_file).convert('RGBA'))
 height, width, channels = original.shape
 original.dtype = np.uint32
 buffer = original.flatten()
@@ -16,17 +19,21 @@ queue = cl.CommandQueue(ctx)
 
 mf = cl.mem_flags
 d_orig = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=buffer)
-d_dest = cl.Buffer(ctx, mf.WRITE_ONLY, buffer.nbytes)
+d_dest = cl.Buffer(ctx, mf.WRITE_ONLY, width*height)
 
 prg = cl.Program(ctx, """
     __kernel void grayscale(__global const uint* orig,
-        __global uint* dest,
+        __global uchar* dest,
         uint const width, uint const height)
     {
-      int x = get_global_id(0);
-      int y = get_global_id(1);
-      int pos = x + y*width;
-      dest[pos] = orig[pos];
+    int pos = get_global_id(0);
+    int y = pos / width;
+    int x = pos - (y*width);
+    uchar* colors = (uchar *) &(orig[pos]);
+    uchar r = colors[0];
+    uchar g = colors[1];
+    uchar b = colors[2];
+    dest[pos] = .299f * r + .587f * g + .114f * b;
     }
     """).build()
 
@@ -34,11 +41,10 @@ prg.grayscale(
     queue, buffer.shape, None, d_orig, d_dest,
     np.uint32(width), np.uint32(height))
 
-result = np.empty_like(buffer)
+result = np.empty((height, width), dtype=np.uint8)
 cl.enqueue_copy(queue, result, d_dest)
-result.dtype = np.uint8
-result.shape = (height, width, channels)
 
-surface = cairo.ImageSurface.create_for_data(
-    result, cairo.FORMAT_ARGB32, width, height)
-surface.write_to_png('../data/grey_butner.png')
+# Image read flips y-axis... flipping back now
+# The copy is because we require the underlying data to be ordered right
+# Image.frombuffer('RGBA', (width, height), result[::-1, :, :].copy()).save('../output/grey_butner.png')
+Image.frombuffer('L', (width, height), result[::-1, :].copy()).save('../output/grey_%s' % os.path.basename(target_file))
